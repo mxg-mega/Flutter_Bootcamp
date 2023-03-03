3ff770a4d2eba7e6675f7de2bca06703045c8b3b
mxg-mega <muhammadmasanawa75@gmail.com>
2023-03-03 19:55:02 +0100

    updates

--- lib/result.dart
+++ lib/result.dart
@@ -4,9 +4,9 @@ import 'dart:core';
 void main()
 {
   int passMark = 60;
-  int score, average;
+  int? score, average;
 
-  print("what is the student's score: ");
+  print("what is the student's percentage: ");
   score = stdin.readByteSync();
 
   print("what is the class average: ");
@@ -14,11 +14,19 @@ void main()
 
   if(score >= passMark)
   {
-    print("you pass ;)");
+    print("You pass, The Student has an 'A , Excellent\n");
+  }
+  if(score == average)
+  {
+    print("You pass, It's an average score you can do better\n");
+  }
+  else if(score >= (average - 5))
+  {
+     print("You pass, Almost there, try harder next time");
   }
   else
   {
-    print("try Harder next time but you failed :(");
+    print("you failed,try Harder next time :( \n");
   }
 
 }
