80008614eb4c38b8855478bf8ff2bb475c56157f
mxg-mega <muhammadmasanawa75@gmail.com>
2023-03-04 17:54:54 +0100

    finished

--- lib/result.dart
+++ lib/result.dart
@@ -4,29 +4,32 @@ import 'dart:core';
 void main()
 {
   int passMark = 60;
-  int? score, average;
+  int score, average;
 
-  print("what is the student's percentage: ");
-  score = stdin.readByteSync();
 
-  print("what is the class average: ");
-  average = stdin.readByteSync();
+  print("what is the student's percentage: \n");
+  //Getting an int from the user
+  var input1 = stdin.readLineSync();
+  score = int.parse(input1 ?? '0');
 
-  if(score >= passMark)
+  print("what is the class average: \n");
+  //getting second int from user
+  var input2 = stdin.readLineSync();
+  average = int.parse(input2 ?? '0');
+
+  if(score > passMark)
   {
-    print("You pass, The Student has an 'A , Excellent\n");
-  }
-  if(score == average)
-  {
-    print("You pass, It's an average score you can do better\n");
+    print("Excellent! You pass, your score is $score and the average is $average\n");
   }
   else if(score >= (average - 5))
   {
-     print("You pass, Almost there, try harder next time");
+    print("You pass, Almost there, try harder next time\nyour score is $score and the average is $average\n");
+  }
+  else if(score == average) {
+    print("You pass, It's an average score you can do better\nyour score is $score and the average is $average\n");
   }
   else
   {
-    print("you failed,try Harder next time :( \n");
+    print("you failed,try Harder next time :( \nyour score is $score and the average is $average\n");
   }
-
 }
